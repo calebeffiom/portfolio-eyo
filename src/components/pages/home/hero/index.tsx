d7f4427a6fbcbd36bdae62c76a221e56a039729f
calebeffiom <calebeffiom974@gmail.com>
2025-04-18 17:41:41 +0100

    changes to hero text

--- src/components/pages/home/hero/index.tsx
+++ src/components/pages/home/hero/index.tsx
@@ -7,7 +7,7 @@ const HomeHero = () => {
                 <Container>
                     <section className="hero-content-cont">
                         <div className="hero-headingText-cont w-[100%] mb-[60px] max-sm:mb-[30px] sm:mb-[30px]">
-                            <h2 className="text-[3.5rem] leading-tight max-sm:text-[1.3rem] sm:text-[1.5rem] md:text-[2rem] lg:text-[3rem] xl:text-[3.5rem] bg-gradient-to-r from-gray-900 to-gray-700 bg-clip-text text-transparent">Hi, I'm Emmanuel Eyo, a Growth<br className="max-xs:hidden"/>Strategist, Revenue Hacker &<br className="max-xs:hidden"/>Your Next Competitive<br className="max-xs:hidden"/>Advantage.</h2>
+                            <h2 className="text-[3.5rem] leading-tight max-sm:text-[1.3rem] sm:text-[1.5rem] md:text-[2rem] lg:text-[3rem] xl:text-[3.5rem] bg-gradient-to-r from-gray-900 to-gray-700 bg-clip-text text-transparent">Hi, I'm Emmanuel Eyo, a Growth<br className="max-xs:hidden"/>Strategist, Revenue Hacker & <br className="max-xs:hidden"/>Your Next Competitive <br className="max-xs:hidden"/>Advantage.</h2>
                         </div>
                         <div className="hero-image-cont w-[100%] h-[740px] max-sm:h-[300px] sm:h-[350px] md:h-[450px] xl:h-[740px]">
                             <img src="/img/work2.jpg" className="block w-[100%] h-[100%] object-cover rounded-[40px]" alt="" />
